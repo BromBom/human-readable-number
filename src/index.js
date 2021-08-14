be807d2fe0f4fd09db14a835dfcf0ea83d0b838f
Ihar Levachkov <pokerlis@gmail.com>
2021-08-14 22:57:22 +0300

    fix: fix space in string

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ module.exports = function toReadable(number) {
     let dec = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
     let tenTh = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
 
-    
-
     if (num == 0) {
        return 'zero';
     }
@@ -15,11 +13,11 @@ module.exports = function toReadable(number) {
     }
 
     else if (num.length === 2) {
-        return +num[0] === 1 ? `${tenTh[+num[1]]}` : `${dec[+num[0]]} ${uno[+num[1]]}`;
+        return (+num[0] === 1 ? `${tenTh[+num[1]]}` : `${dec[+num[0]]} ${uno[+num[1]]}`).split('  ').join(' ').trim();
     }
 
     else if (num.length === 3) {
-        return +num[1] === 1 ? `${uno[+num[0]]} hundred ${tenTh[+num[2]]}` : `${uno[+num[0]]} hundred ${dec[+num[1]]} ${uno[+num[2]]}`
+        return (+num[1] === 1 ? `${uno[+num[0]]} hundred ${tenTh[+num[2]]}` : `${uno[+num[0]]} hundred ${dec[+num[1]]} ${uno[+num[2]]}`).split('  ').join(' ').trim();
     }
     
 }
